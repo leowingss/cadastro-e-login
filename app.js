c5ed9bc97446a0f8a6e9cf37608db3fb0fae4cca
Leonardo Santos Gonçales <leondani853@gmail.com>
2021-06-20 03:24:49 -0300

    feat: configurando session no arquivo app

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const session = require('express-session')
 
 const indexRouter = require('./routes/index');
 const usuariosRouter = require('./routes/usuarios');
@@ -13,6 +14,11 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(session({
+    secret: 'Mensagem secreta',
+    resave: true,
+    saveUninitialized: true
+}))
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
